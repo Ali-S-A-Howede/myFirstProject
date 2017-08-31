92bb9513ea59b92dddae19b846f6a5d1e9e00344
Ali-S-A-Howede <31488383+Ali-S-A-Howede@users.noreply.github.com>
2017-08-31 11:37:59 +0300

    variables
    first programs about decaring varibles

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+            double myDouble = 5.5;
+            string myString = "Ali";
+            bool myBool = true;
+            char myChar = 'a';
+
+        }
+    }
+}
