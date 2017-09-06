d4a7ec24348603f11383f4b3fdba11b014e18cf7
Ali-S-A-Howede <31488383+Ali-S-A-Howede@users.noreply.github.com>
2017-09-06 09:32:53 +0300

    addcode
    add another code

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -16,6 +16,18 @@ namespace Variables
             bool myBool = true;
             char myChar = 'a';
 
+            //If we want to know the range of the varible float or any varibles by code
+            //float minFloat = float.MinValue;
+            //float maxFlaot = float.MaxValue;
+            //Console.WriteLine("Min Value={0} and Max value={1}", minFloat, maxFlaot);
+            //Console.ReadKey();
+
+            float floatVar = 1f / 3;
+            double doubleVar = 1d / 3;
+            decimal decimalVar = 1m / 3;
+            Console.WriteLine("float Value={0}\n  double Value={1}\n decimal value={2}", floatVar, doubleVar,decimalVar);
+            Console.ReadKey();
+
         }
     }
 }
