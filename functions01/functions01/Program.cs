81a678af1158b284b3c10a47276e31aee635a200
Ali-S-A-Howede <31488383+Ali-S-A-Howede@users.noreply.github.com>
2017-09-09 05:39:16 +0300

    functionNo1
    learn about create functions

---
+++ functions01/functions01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions01
+{
+    class Program
+    {
+        static void printName()
+        {
+            Console.Write("my name is Ali S A Howede");
+        }
+        static void Main(string[] args)
+        {
+            printName();
+            Console.ReadKey();
+        }
+    }
+}
