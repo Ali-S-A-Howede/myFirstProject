1cf1a6d172d931ccd31044a48fd6341372ab1d90
Ali-S-A-Howede <31488383+Ali-S-A-Howede@users.noreply.github.com>
2017-09-09 08:45:44 +0300

    returnFunction
    about return args to the function then activate codes then return the results to the main function

---
+++ returnFunction/returnFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace returnFunction
+{
+    class Program
+    {
+        static double addNumbers2 (double myfirstNumber,double mySecondNumber)
+        {
+            double temp = myfirstNumber + mySecondNumber;
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            double firstNumber = 17,secondNumber = 23;
+            //int temp;
+            //addNumbers2(number1, number2);
+            //temp = addNumbers2(number1 + number2);
+            Console.WriteLine($"{addNumbers2 (firstNumber , secondNumber)}");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
